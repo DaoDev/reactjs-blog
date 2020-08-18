1e6c92a845176e111dceddc95a006cdfb5f0d167
DaoDev <boynguyen1998@gmail.com>
2020-08-18 14:25:04 +0700

    the second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './container/Home';
+import Header from './components/Header';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Header/>
+      <Hero/>
+      <Home/>
     </div>
   );
 }
---
+++ src/container/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css'
+import App from '../../App';
+
+const Home = props =>{
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Home;
