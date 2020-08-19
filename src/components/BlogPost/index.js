962d48ee5d6657675006c5a743b3787feeb1d0ca
DaoDev <boynguyen1998@gmail.com>
2020-08-19 17:36:59 +0700

    update blog commit

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './style.css'
 import Card from '../UI/Card'
+import blogPost from '../../data/blog.json'
 /**
 * @author
 * @function BlogPost
 **/
 
 const BlogPost = (props) => {
+
+  const[post, setPost] = useState({})
+  const[postId, setPostId] = useState('')
+
+useEffect(()=>{
+  const postId = props.match.params.postId;
+  const post = blogPost.data.find(post=>post.id == postId);
+  setPost(post);
+  setPostId(postId)
+}, [post, props.match.params.postId]);
+
   return(
     <div className="blogPostContainer">
         <Card>
             <div className="blogHeader">
-            <span className="blogCategory">Featured</span>
-            <h1 className="postTitle">Beautiful is aways beautuful</h1>
-            <span className="postBy">posted on July 21, 2020 by Sora Blogging Tips</span>
+            <span className="blogCategory">{post.blogCategory}</span>
+          <h1 className="postTitle">{post.blogTitle}</h1>
+            <span className="postBy">posted on {post.postedOn} by {post.author}</span>
+            </div>
+
+            <div className="postImageContainer">
+              <img src={require('../../blogPostImanges/4.jpg')} alt="Post Image" />
+            </div>
+
+            <div className="postContent">
+              <h3>{post.blogTitle}</h3>
+              <p>{post.blogText}</p>
             </div>
         </Card>
     </div>
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,18 +1,66 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './style.css'
 import Card from '../UI/Card'
+import blogPost from '../../data/blog.json'
+import { NavLink } from 'react-router-dom'
 /**
 * @author
 * @function Sidebar
 **/
 
 const Sidebar = (props) => {
-  return(
-    <Card width="27%">
-        Sidebar
-    </Card>
-   )
 
+  const[post, setPost] = useState([])
+    useEffect(()=>{
+      // const postId = props.match.params.postId;
+      const post = blogPost.data
+      setPost(post);
+    }, post);
+
+  return(
+    <div className="sidebarContainer">
+        <Card style={{marginBottom:"20px",padding:'20px', boxSizing:'border-box'}}>
+          <div className="cardHeader">
+              <span>About Us</span>
+          </div>
+          <div className="profileImageContainer">
+            <img src={require('../../blogPostImanges/maxresdefault.jpg')} />
+          </div>
+          <div className="cardBody">
+            <p className="personalBio">My name is Dao, I am a developer speciallzation in Front end developement</p>
+          </div>
+        </Card>
+
+        <Card style={{marginBottom:"20px",padding:'20px', boxSizing:'border-box'}}>
+          <div className="cardHeader">
+              <span>Social Network</span>
+          </div>
+        </Card>
+
+        <Card style={{marginBottom:"20px",padding:'20px', boxSizing:'border-box'}}>
+          <div className="cardHeader">
+              <span>Recent Posts</span>
+          </div>
+
+          <div className="recentPosts">
+              {
+                post.map(post =>{
+                  return (
+                    <NavLink to={`/post/${post.id}`}>
+                        <div className="recentPost">
+                          <div className="recentPost">
+                              <h3>{post.blogTitle}</h3>
+                              <span>{post.postedOn}</span>
+                          </div> 
+                        </div>
+                    </NavLink>
+                  );
+                })
+              }
+          </div>
+        </Card>
+    </div>
+   )
  }
 
 export default Sidebar
